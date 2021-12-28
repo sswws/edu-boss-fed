bc3c9f27ae2e0b7fc459a4d25ada669f9429d33e
韩昊洋 <hanhaoyang@192.168.0.7>
2021-12-28 15:19:25 +0800

    first commit

--- src/views/resource/components/List.vue
+++ src/views/resource/components/List.vue
@@ -3,28 +3,60 @@
         <el-card class="box-card">
             <div slot="header" class="clearfix">
                 <!-- 使用 Form 组件: 行内表单 -->
-                <el-form :inline="true" :model="form" class="demo-form-inline">
-                    <el-form-item label="资源名称">
-                        <el-input v-model="form.name" placeholder="资源名称"></el-input>
+                <el-form
+                :inline="true"
+                :model="form"
+                class="demo-form-inline"
+                ref="form"
+                >
+                    <el-form-item label="资源名称" prop="name">
+                        <el-input
+                          v-model="form.name"
+                          placeholder="资源名称"
+                          clearable
+                        ></el-input>
                     </el-form-item>
-                    <el-form-item label="资源路径">
-                        <el-input v-model="form.url" placeholder="资源路径"></el-input>
+                    <el-form-item label="资源路径" prop="url">
+                        <el-input
+                          v-model="form.url"
+                          placeholder="资源路径"
+                          clearable
+                        ></el-input>
                     </el-form-item>
-                    <el-form-item label="资源分类">
-                        <el-select v-model="form.categoryId" placeholder="资源分类">
-                            <el-option label="区域一" value="shanghai"></el-option>
-                            <el-option label="区域二" value="beijing"></el-option>
+                    <el-form-item label="资源分类" prop="categoryId">
+                        <el-select
+                          v-model="form.categoryId"
+                          placeholder="资源分类"
+                          clearable
+                        >
+                          <!-- 请求接口进行下拉菜单项设置 -->
+                          <el-option
+                            v-for="item in resourceCategories"
+                            :key="item.id"
+                            :label="item.name"
+                            :value="item.id"
+                          ></el-option>
                         </el-select>
                     </el-form-item>
                     <el-form-item>
-                        <el-button type="primary">查询</el-button>
+                      <el-button
+                        @click="onReset"
+                      >重置
+                      </el-button>
+                        <el-button
+                          type="primary"
+                          :disabled="isLoading"
+                          @click="onSubmit"
+                        >查询</el-button>
                     </el-form-item>
                 </el-form>
             </div>
                 <!-- 使用 Table 组件 -->
                 <el-table
                   :data="resources"
-                  style="width: 100%">
+                  style="width: 100%"
+                  v-loading="isLoading"
+                  >
                 <el-table-column
                   type="index"
                   label="编号"
@@ -72,14 +104,16 @@
                   :page-sizes="[10, 15, 20]"
                   :page-size="form.size"
                   layout="total, sizes, prev, pager, next, jumper"
-                  :total="totalCount">
+                  :total="totalCount"
+                  :disabled="isLoading"
+                  >
                </el-pagination>
         </el-card>
     </div>
 </template>
 
 <script>
-import { getResourcePages } from '@/services/resource'
+import { getResourcePages, getResourceCategories } from '@/services/resource'
 export default {
   name: 'ResourceList',
   data () {
@@ -99,13 +133,37 @@ export default {
         categoryId: ''
       },
       // 数据总数
-      totalCount: 0
+      totalCount: 0,
+      // 存储资源分类信息
+      resourceCategories: [],
+      // 用于保存加载状态
+      isLoading: false
     }
   },
   created () {
+    // 加载资源数据
     this.loadResources()
+    // 加载资源列表
+    this.loadResourceCategories()
   },
   methods: {
+    // 重置按钮点击事件
+    onReset () {
+      this.$refs.form.resetFields()
+    },
+    // 提交筛选功能
+    onSubmit () {
+      // 请求数据前，将请求的页数更新为 1
+      this.form.current = 1
+      this.loadResources()
+    },
+    // 加载资源列表
+    async loadResourceCategories () {
+      const { data } = await getResourceCategories()
+      if (data.code === '000000') {
+        this.resourceCategories = data.data
+      }
+    },
     // 每条显示条数变化时触发
     handleSizeChange (val) {
       this.form.size = val
@@ -119,13 +177,13 @@ export default {
       this.loadResources()
     },
     async loadResources () {
-      const { data } = await getResourcePages({
-        current: this.form.current,
-        size: this.form.size
-      })
+      // 开始加载数据
+      const { data } = await getResourcePages(this.form)
       if (data.code === '000000') {
         this.resources = data.data.records
         this.totalCount = data.data.total
+        // 取消加载状态
+        this.isLoading = false
       }
     },
     handleEdit () {
